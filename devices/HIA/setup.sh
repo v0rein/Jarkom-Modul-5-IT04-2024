b5f05ee6488adef4603675332cf8f84c039e08d5
AthallaITS <5027231018@student.its.ac.id>
2024-12-01 06:24:59 +0700

    refactor: comments iptables command in HIA

--- devices/HIA/setup.sh
+++ devices/HIA/setup.sh
@@ -9,21 +9,19 @@ service apache2 restart
 #iptables -A INPUT -s 10.72.0.128/25 -m time --timestart 08:00 --timestop 21:00 -j ACCEPT
 #iptables -A INPUT -s 10.72.1.0/24 -m time --timestart 03:00 --timestop 23:00 -j ACCEPT
 
-#iptables -A INPUT -m state --state NEW -m recent --set --name PORTSCAN --rsource
-#iptables -A INPUT -m state --state NEW -m recent --update --seconds 10 --hitcount 25 --name PORTSCAN --rsource -j DROP
 #Create a chain for handling port scanning
-iptables -N PORTSCAN
+#iptables -N PORTSCAN
 
 #Detect and handle new connections to ports 1-100
-iptables -I INPUT 1 -j LOG --log-prefix "PORT SCAN BRO: " --log-level 4 --log-tcp-options --log-ip-options
-iptables -A INPUT -p tcp --dport 1:100 -m state --state NEW -m recent --set --name portscan
-iptables -A INPUT -p tcp --dport 1:100 -m state --state NEW -m recent --update --seconds 10 --hitcount 25 --name portscan -j PORTSCAN
+#iptables -I INPUT 1 -j LOG --log-prefix "PORT SCAN BRO: " --log-level 4 --log-tcp-options --log-ip-options
+#iptables -A INPUT -p tcp --dport 1:100 -m state --state NEW -m recent --set --name portscan
+#iptables -A INPUT -p tcp --dport 1:100 -m state --state NEW -m recent --update --seconds 10 --hitcount 25 --name portscan -j PORTSCAN
 
 #Log and block port-scanning IPs
-iptables -A PORTSCAN -m recent --set --name blacklist
-iptables -I PORTSCAN 1 -j LOG --log-prefix "PORT SCAN DETECTED: " --log-level 4 --log-tcp-options --log-ip-options
-iptables -A PORTSCAN -j DROP
+#iptables -A PORTSCAN -m recent --set --name blacklist
+#iptables -I PORTSCAN 1 -j LOG --log-prefix "PORT SCAN DETECTED: " --log-level 4 --log-tcp-options --log-ip-options
+#iptables -A PORTSCAN -j DROP
 
 #Block all further traffic from blacklisted IPs
-iptables -A INPUT -m recent --name blacklist --rcheck -j DROP
-iptables -A OUTPUT -m recent --name blacklist --rcheck -j DROP
+#iptables -A INPUT -m recent --name blacklist --rcheck -j DROP
+#iptables -A OUTPUT -m recent --name blacklist --rcheck -j DROP
