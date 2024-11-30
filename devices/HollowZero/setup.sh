9a101aa413b27bab91b7133f209c03c6ae0a4ff8
AthallaITS <5027231018@student.its.ac.id>
2024-12-01 06:24:15 +0700

    fix: hollowzero setup.sh

--- devices/HollowZero/setup.sh
+++ devices/HollowZero/setup.sh
@@ -9,11 +9,7 @@ service apache2 restart
 #iptables -A INPUT -s 10.72.0.128/25 -m time --timestart 08:00 --timestop 21:00 -j ACCEPT
 #iptables -A INPUT -s 10.72.1.0/24 -m time --timestart 03:00 --timestop 23:00 -j ACCEPT
 
-#iptables -A INPUT -m state --state NEW -m recent --set --name PORTSCAN --rsource
-#iptables -A INPUT -m state --state NEW -m recent --update --seconds 10 --hitcount 25 --name PORTSCAN --rsource -j DROP
-#BELOM SELESAI
-
 # Allow only 2 active connections
-iptables -A INPUT -p tcp --dport http -m conntrack --ctstate NEW -m recent --set
-iptables -A INPUT -p tcp --dport http -m conntrack --ctstate NEW -m recent --update --seconds 1 --hitcount 3 -j REJECT
-iptables -A INPUT -p tcp --dport http -j ACCEPT
+#iptables -A INPUT -p tcp --dport http -m conntrack --ctstate NEW -m recent --set
+#iptables -A INPUT -p tcp --dport http -m conntrack --ctstate NEW -m recent --update --seconds 1 --hitcount 3 -j REJECT
+#iptables -A INPUT -p tcp --dport http -j ACCEPT
